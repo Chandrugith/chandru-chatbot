554a6dc89fa9ed38e503dc5e0c72fc3f71a38fad
Chandrugith <104364117+Chandrugith@users.noreply.github.com>
2024-01-07 21:38:04 +0530

    Update script.js

--- script.js
+++ script.js
@@ -82,7 +82,7 @@ function waitAndResponce(inputText) {
     switch (inputText.toLowerCase().trim()) {
         case "intro":
             setTimeout(() => {
-                sendTextMessage("Hello there 👋🏻,<br><br>My name is <span class='bold'><a class='alink'>chandru</a>.</span><br><br>I am a Computer Engineering student at <span class='bold'>Mangalore Institute of Technology and Engineering👨🏻‍💻📚</span><br><br>I am eager to hear about potential career opportunities, so I would be pleased to chat about job openings in the engineering sphere.<br><br>Send <span class='bold'>'help'</span> to know more about me.<br>");
+                sendTextMessage("Hello there 👋🏻,<br><br>My name is <span class='bold'><a class='alink'>chandru</a>.</span><br><br>I am a Computer Engineering student at <span class='bold'>Muthayammal engineering college information technology </span><br><br>I am eager to hear about potential career opportunities, so I would be pleased to chat about job openings in the engineering sphere.<br><br>Send <span class='bold'>'help'</span> to know more about me.<br>");
             }, 2000);
             break;
         
